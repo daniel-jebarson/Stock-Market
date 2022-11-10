f1520d7453559f3da62b22192144ceb0f5b19a00
daniel-jebarson <danieljebarson21@gmail.com>
2022-11-11 00:13:30 +0530

    Add reg comp 👾

---
+++ frontend/src/components/registration-page/Registration.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "./RegNav/RegNav";
+import "./Registration.css";
+import Sample_reg from "./Sample_reg";
+
+function Registration() {
+  return (
+    <div>
+      <NavBar />
+      {/* <RegForm /> */}
+      <Sample_reg />
+
+      {/* <Sample_login /> */}
+    </div>
+  );
+}
+
+export default Registration;
