332909c60c14cc29bdc05c1eb68ba7cff31c12a6
daniel-jebarson <danieljebarson21@gmail.com>
2022-11-11 00:24:31 +0530

    Add Home Page ✔️

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import LoginGraph from "../components/landing_page/div1/LoginGraph";
+
+import HowtoPlay from "../components/landing_page/div2/HowtoPlay";
+
+import LineChart from "../components/landing_page/div3/LineChart";
+
+const HomePage = () => {
+  return (
+    <div>
+      <LoginGraph />
+      <HowtoPlay />
+      <LineChart />
+    </div>
+  );
+};
+
+export default HomePage;
