22e4b50735d10fa3b0fb338096e029f687d447b1
daniel-jebarson <danieljebarson21@gmail.com>
2022-11-11 00:24:18 +0530

    Add MainPage 🔃

---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import BuySellMainPage from "../components/GraphMainPage/BuySellMainPage";
+
+const MainPage = () => {
+  return (
+    <div >
+      <BuySellMainPage />
+    </div>
+  );
+};
+
+export default MainPage;
