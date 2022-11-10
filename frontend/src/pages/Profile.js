5a113120f5ef061159cdad3b3fbc60f1ffd25f1d
daniel-jebarson <danieljebarson21@gmail.com>
2022-11-11 00:24:04 +0530

    Added profile page 🍥

---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MyProfile from "../components/profilePage/MyProfile/MyProfile";
+
+const Profile = () => {
+  return (
+    <div>
+      <MyProfile />
+    </div>
+  );
+};
+
+export default Profile;
