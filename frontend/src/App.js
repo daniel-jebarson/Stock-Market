a6f70f7761fbd6bd91118beae88ce95b1eae24dc
daniel-jebarson <danieljebarson21@gmail.com>
2022-11-11 00:10:06 +0530

    Add App file ♻️

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import React from "react";
 
-function App() {
+import HomePage from "./pages/HomePage";
+import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
+import MainPage from "./pages/MainPage";
+import AdminP from "./pages/AdminPage";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Registration from "./pages/Registration";
+
+const App = () => {
+  const [active, setActive] = useState(sessionStorage.getItem("active"));
+
+  if (new Date().getTime() - active > 100 * 60 * 1000 || !active) {
+    sessionStorage.clear();
+    // alert("Session expired!");
+    //x*60*1000 = x minutes
+  } else {
+    // setActive(new Date().getTime());
+    sessionStorage.setItem("active", new Date().getTime());
+  }
+  const [username, setUsername] = useState(sessionStorage.getItem("username"));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        <Route
+          path="/dashboard"
+          element={
+            !username ? (
+              <Navigate to="/registration?logout=true" />
+            ) : (
+              <Dashboard />
+            )
+          }
+        />
+        <Route path="/registration" element={<Registration />} />
+        <Route
+          path="/MainPage"
+          element={
+            !username ? (
+              <Navigate to="/registration?logout=true" />
+            ) : (
+              <MainPage />
+            )
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            !username ? (
+              <Navigate to="/registration?logout=true" />
+            ) : (
+              <Profile />
+            )
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            !username ? <Navigate to="/registration?logout=true" /> : <AdminP />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
