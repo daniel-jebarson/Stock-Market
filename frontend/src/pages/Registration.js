aabc574ae1cc36c5f24b6ee520b3afc7154517ab
daniel-jebarson <danieljebarson21@gmail.com>
2022-11-11 00:23:46 +0530

    Add reg page ✔️

---
+++ frontend/src/pages/Registration.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Reg from "../components/registration-page/Registration";
+
+function Registration() {
+  return (
+    <div>
+      <Reg />
+    </div>
+  );
+}
+
+export default Registration;
