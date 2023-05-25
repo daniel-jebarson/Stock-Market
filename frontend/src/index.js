a7136f7e5381b4597882396ad5b97a54e6c723b1
daniel-jebarson <danieljebarson21@gmail.com>
2023-05-25 10:55:38 +0530

    💫 Add: Analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { Analytics } from "@vercel/analytics/react";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics
+      beforeSend={(event) => {
+        if (event.url.includes("localhost")) {
+          return null;
+        }
+        return event;
+      }}
+    />
   </React.StrictMode>
 );
 
